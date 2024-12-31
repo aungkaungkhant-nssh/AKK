9f4955bd03079d45543da015dc8c995b70b9f780
aungkaungkhant-nssh <akkgit0909@gmail.com>
2024-12-31 14:45:47 +0630

    project ui fix

--- components/project/ProjectCard.tsx
+++ components/project/ProjectCard.tsx
@@ -28,43 +28,46 @@ export default function ProjectCard({ project }: PropsTypes) {
         2: project.demoLink || "",
     };
     return (
-        <div className="bg-background text-tertiary px-6 py-5 rounded-lg shadow-lg shadow-primary/20 w-full md:w-[48%] lg:w-[30%]  min-h-[200px] relative transition-transform transform hover:scale-105">
-            <div className="flex items-start justify-between gap-4">
-                <div className="flex-1">
-                    <Link href={linkMapping[0] || ""}>
-                        <h1 className="text-xl text-white font-bold hover:underline">{project.title}</h1>
-                    </Link>
-                    <p className="text-tertiary tracking-wide text-justify mt-2 leading-relaxed">
-                        {project.subTitle}
-                    </p>
-                </div>
-                {project.logo && (
-                    <div className="flex-shrink-0">
-                        <Image
-                            src={project.logo as string}
-                            alt={`${project.title} logo`}
-                            className="border border-gray-200 rounded-lg shadow-md"
-                            width={80}
-                            height={80}
-                        />
+        <div className="bg-background text-tertiary px-6 py-5 rounded-lg shadow-lg shadow-primary/20 w-full md:w-[48%] lg:w-[30%]  md:min-h-[200px]  transition-transform transform hover:scale-105">
+            <div className="flex flex-col justify-between items-center">
+                <div className="flex items-start justify-between gap-4">
+                    <div className="flex-1">
+                        <Link href={linkMapping[0] || ""}>
+                            <h1 className="text-xl text-white font-bold hover:underline">{project.title}</h1>
+                        </Link>
+                        <p className="text-tertiary tracking-wide text-justify mt-2 leading-relaxed">
+                            {project.subTitle}
+                        </p>
                     </div>
-                )}
+                    {project.logo && (
+                        <div className="flex-shrink-0">
+                            <Image
+                                src={project.logo as string}
+                                alt={`${project.title} logo`}
+                                className="border border-gray-200 rounded-lg shadow-md"
+                                width={80}
+                                height={80}
+                            />
+                        </div>
+                    )}
+                </div>
+
+                <div className="mt-4  flex gap-3">
+                    {iconLinks.map((link, index) => {
+                        link.link = linkMapping[index] || "";
+                        return (
+                            <LinkItem
+                                key={index}
+                                icon={link.icon}
+                                link={link.link}
+                                color={link.color}
+                                size={20}
+                            />
+                        );
+                    })}
+                </div>
             </div>
 
-            <div className="mt-4 absolute bottom-3 left-1/2 transform -translate-x-1/2 flex gap-3">
-                {iconLinks.map((link, index) => {
-                    link.link = linkMapping[index] || "";
-                    return (
-                        <LinkItem
-                            key={index}
-                            icon={link.icon}
-                            link={link.link}
-                            color={link.color}
-                            size={20}
-                        />
-                    );
-                })}
-            </div>
         </div>
 
     );
